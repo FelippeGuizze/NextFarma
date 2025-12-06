9941ef2089c87b9086001a04f70a5b2ce87cad9e
Felippe Guizze <eunaoodeiorato@gmail.com>
2025-12-06 17:32:11 -0300

    Inicio da implementação do validator

---
+++ NextFarma/Controllers/ValidadorController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace NextFarma.Controllers
+{
+    // O nome do Controller deve terminar em "Controller"
+    public class ValidadorController : Controller
+    {
+        // O método Index() é a ação padrão que responde à URL /Validador
+        public IActionResult Index()
+        {
+            // Retorna a View localizada em Views/Validador/Index.cshtml
+            return View();
+        }
+
+        // Se você precisar de lógica de processamento aqui (ex: Validação, Submissão), ela viria aqui:
+        /*
+        [HttpPost]
+        public IActionResult ValidarPrescricao(SeuModeloDeDados modelo)
+        {
+            // Lógica de validação...
+            return View("Index", modelo);
+        }
+        */
+    }
+}
